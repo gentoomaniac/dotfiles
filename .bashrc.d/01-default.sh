59b43c7f8c979670fee83bdc299e0da2d8736e9e
Marco Siebecke <marco.siebecke@embark-studios.com>
2024-12-14 00:05:57 +0100

    change prompt to powerline-go

--- .bashrc.d/01-default.sh
+++ .bashrc.d/01-default.sh
@@ -2,69 +2,7 @@ if [ -f /etc/lsb-release ]; then
  source /etc/lsb-release
 fi
 
-
-# prompt
-# https://misc.flogisoft.com/bash/tip_colors_and_formatting
-BLACK="\[$(tput setaf 0)\]"
-RED="\[$(tput setaf 196)\]"
-GREEN="\[$(tput setaf 2)\]"
-GREENB="\[$(tput setaf 118)\]"
-BROWN="\[$(tput setaf 3)\]"
-YELLOW="\[$(tput setaf 227)\]"
-LIGHTBLUE="\[$(tput setaf 033)\]"
-BLUE="\[$(tput setaf 4)\]"
-DARKBLUE="\[$(tput setaf 021)\]"
-MAGENTA="\[$(tput setaf 5)\]"
-CYAN="\[$(tput setaf 6)\]"
-WHITE="\[$(tput setaf 7)\]"
-ORANGE="\[$(tput setaf 202)\]"
-RESET="\[$(tput sgr0)\]"
-BOLD="\[$(tput bold)\]"
-SMUL="\[$(tput smul)\]"
-RMUL="\[$(tput rmul)\]"
-BLINK="\[$(tput blink)\]"
-SMSO="\[$(tput smso)\]"
-RMSO="\[$(tput rmso)\]"
-
-parse_kube_ctx() {
-  cat ~/.kube/config 2> /dev/null | \
-  grep "^current-context:" | \
-  sed "s/^current-context: //" | \
-  grep -v '""' | \
-  sed -E -e "s/tink\.teleport\.sh-//" \
-         -e "s/(minikube)/${CYAN}[\1]${RESET} /" \
-         -e "s/(.+-testing)/${GREEN}[\1]${RESET} /" \
-         -e "s/(.+-(staging|preprod))/${ORANGE}[\1]${RESET} /" \
-         -e "s/(.+-production)/${RED}[\1]${RESET} /"
-         #-e "s/(.+-production)/$(tput setaf 9)[\1]$(tput sgr0)/"
-}
-
-prompt_git() {
-    git branch &>/dev/null || return 1
-    HEAD="$(git symbolic-ref HEAD 2>/dev/null)"
-    BRANCH="${HEAD#refs/heads/}"
-    git status -s | grep -q '^ M' && STATUS+="!"
-    git status -s | grep -q '^??' && STATUS+="+"
-    if [ -z "${STATUS}" ]; then
-        GIT_STATUS="${YELLOW}(git:${BRANCH:-unknown}) "
-    else
-        GIT_STATUS="${YELLOW}(git:${BRANCH:-unknown}:${STATUS}) "
-    fi
-    echo -n "${GIT_STATUS}"
-}
-
-function build_prompt() {
-    PS1="${BOLD}"
-    PS1+="$([ ! -z "${SSH_ENV_PREFIX}" ] && echo "${RED}(${WHITE}${SSH_ENV_PREFIX}${RED}) ")"    # SSH env prefix
-    PS1+="${RED}[${WHITE}\t${RED}] "                                                             # timestamp
-    PS1+="$([ "${UID}" -eq 0 ] && echo "${RED}" || echo "${GREEN}")\u${BLUE}@${GREEN}\h "        # user@host
-    PS1+="${BLUE}\W/ "                                                                           # cwd
-    PS1+="$(prompt_git)"                                                                         # GIT status
-    PS1+="$(parse_kube_ctx)"                                                                     # k8s context
-    PS1+="${BOLD}${YELLOW}>${RESET} "                                                            # prompt indicator
-}
-
-PROMPT_COMMAND=build_prompt
+export GOPATH="${HOME}/.go"
 
 # ssh-agent config
 function start_ssh_agent {
---
+++ .bashrc.d/05-prompt.sh
@@ -0,0 +1,43 @@
+# prompt
+# https://misc.flogisoft.com/bash/tip_colors_and_formatting
+BLACK="\[$(tput setaf 0)\]"
+RED="\[$(tput setaf 196)\]"
+GREEN="\[$(tput setaf 2)\]"
+GREENB="\[$(tput setaf 118)\]"
+BROWN="\[$(tput setaf 3)\]"
+YELLOW="\[$(tput setaf 227)\]"
+LIGHTBLUE="\[$(tput setaf 033)\]"
+BLUE="\[$(tput setaf 4)\]"
+DARKBLUE="\[$(tput setaf 021)\]"
+MAGENTA="\[$(tput setaf 5)\]"
+CYAN="\[$(tput setaf 6)\]"
+WHITE="\[$(tput setaf 7)\]"
+ORANGE="\[$(tput setaf 202)\]"
+RESET="\[$(tput sgr0)\]"
+BOLD="\[$(tput bold)\]"
+SMUL="\[$(tput smul)\]"
+RMUL="\[$(tput rmul)\]"
+BLINK="\[$(tput blink)\]"
+SMSO="\[$(tput smso)\]"
+RMSO="\[$(tput rmso)\]"
+
+if [ ! -f "$GOPATH/bin/powerline-go" ]; then
+    go install github.com/justjanne/powerline-go@latest
+fi
+
+function _powerline() {
+    MODULES="venv,user,host,ssh,cwd,perms,git,kube,hg,jobs,exit,root"
+
+    PS1="$($GOPATH/bin/powerline-go -error $? -jobs $(jobs -p | wc -l) -newline -theme gruvbox -modules ${MODULES} -hostname-only-if-ssh -colorize-hostname)"
+
+    # Uncomment the following line to automatically clear errors after showing
+    # them once. This not only clears the error for powerline-go, but also for
+    # everything else you run in that shell. Don't enable this if you're not
+    # sure this is what you want.
+
+    #set "?"
+}
+
+if [ "$TERM" != "linux" ] && [ -f "$GOPATH/bin/powerline-go" ]; then
+    PROMPT_COMMAND="_powerline;"
+fi
